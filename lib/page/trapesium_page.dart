302134b110ef9d2ac1275c60e2be20382b59df2f
Dhani AM <dhani.a.mm@gmail.com>
2023-01-18 22:04:21 +0800

    added trapesium  logic

--- lib/page/trapesium_page.dart
+++ lib/page/trapesium_page.dart
@@ -20,9 +20,110 @@ class _TrapesiumPageState extends State<TrapesiumPage> {
   final TextEditingController sController = TextEditingController();
   final TextEditingController luasController = TextEditingController();
   final TextEditingController kelilingController = TextEditingController();
+  bool counted = false;
 
   void hitung() {
-    setState(() {});
+    setState(() {
+      String a = aController.text;
+      String b = bController.text;
+      String t = tinggiController.text;
+      String cd = sController.text;
+      String l = luasController.text;
+      String k = kelilingController.text;
+
+      /// if a, b, t known, count L
+      if (a != '' && b != '' && t != '' && l == '') {
+        int alas = int.parse(a);
+        int atap = int.parse(b);
+        int tinggi = int.parse(t);
+        luasController.text = ((alas + atap) * tinggi / 2).toString();
+        counted = true;
+      }
+
+      /// if a, b, cd known, count K
+      if (a != '' && b != '' && cd != '' && k == '') {
+        int alas = int.parse(a);
+        int atap = int.parse(b);
+        int sisiCD = int.parse(cd);
+        kelilingController.text = (alas + atap + sisiCD).toString();
+        counted = true;
+      }
+
+      /// if L, b, t known, count a
+      if (l != '' && b != '' && t != '' && a == '') {
+        int luas = int.parse(l);
+        int atap = int.parse(b);
+        int tinggi = int.parse(t);
+        aController.text = (((luas * 2) - (tinggi * atap)) / tinggi).toString();
+        counted = true;
+      }
+
+      /// if L, a, t known, count b
+      if (l != '' && a != '' && t != '' && b == '') {
+        int luas = int.parse(l);
+        int alas = int.parse(a);
+        int tinggi = int.parse(t);
+        bController.text = (((luas * 2) - (tinggi * alas)) / tinggi).toString();
+        counted = true;
+      }
+
+      /// if L, a, b known, count t
+      if (l != '' && a != '' && b != '' && t == '') {
+        int luas = int.parse(l);
+        int alas = int.parse(a);
+        int atap = int.parse(b);
+        tinggiController.text = ((luas * 2) / (alas + atap)).toString();
+        counted = true;
+      }
+
+      /// if K, b, cd known, count a
+      if (k != '' && b != '' && cd != '' && a == '') {
+        int keliling = int.parse(k);
+        int atap = int.parse(b);
+        int sisi = int.parse(cd);
+        aController.text = (keliling - (atap + sisi + sisi)).toString();
+        counted = true;
+      }
+
+      /// if K, a, cd known, count b
+      if (k != '' && a != '' && cd != '' && b == '') {
+        int keliling = int.parse(k);
+        int alas = int.parse(a);
+        int sisi = int.parse(cd);
+        bController.text = (keliling - (alas + sisi + sisi)).toString();
+        counted = true;
+      }
+
+      /// if K, a, b known, count cd
+      if (k != '' && a != '' && b != '' && cd == '') {
+        int keliling = int.parse(k);
+        int alas = int.parse(a);
+        int atap = int.parse(b);
+        sController.text = ((keliling - (alas + atap)) / 2).toString();
+        counted = true;
+      }
+
+      // error input state
+      if (counted == false) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Salah input'),
+              content: const Text('Tidak bisa menghitung :('),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Ok'),
+                )
+              ],
+            );
+          },
+        );
+      }
+    });
   }
 
   void reset() {
@@ -33,6 +134,7 @@ class _TrapesiumPageState extends State<TrapesiumPage> {
       sController.text = '';
       luasController.text = '';
       kelilingController.text = '';
+      counted = false;
     });
   }
 
